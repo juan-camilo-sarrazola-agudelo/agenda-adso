48237008258e40335be77af3ea54d416dfbe274f
juan camilo sarrazola <jcsarrazola@gmail.com>
2025-11-26 19:50:24 -0500

    Clase_11_Agenda_ADSO_v9_Editar_Contactos_Cierre_ABP

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,7 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
